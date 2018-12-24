1ce46a7245c987830734388705f62f7a4e30e1ba
amirnakic <anakic1@etf.unsa.ba>
2018-12-24 01:14:35 +0100

    Implementirana je klasa GradDTO tako da slijedi JavaBeans specifikacije.

--- src/ba/unsa/etf/rpr/GradDTO.java
+++ src/ba/unsa/etf/rpr/GradDTO.java
@@ -1,4 +1,40 @@
 package ba.unsa.etf.rpr;
 
 public class GradDTO {
+    private String naziv;
+    private int brojStanovika;
+    private DrzavaDTO drzava;
+
+    public GradDTO() {
+    }
+
+    public GradDTO(String naziv, int brojStanovika, DrzavaDTO drzava) {
+        this.naziv = naziv;
+        this.brojStanovika = brojStanovika;
+        this.drzava = drzava;
+    }
+
+    public String getNaziv() {
+        return naziv;
+    }
+
+    public void setNaziv(String naziv) {
+        this.naziv = naziv;
+    }
+
+    public int getBrojStanovika() {
+        return brojStanovika;
+    }
+
+    public void setBrojStanovika(int brojStanovika) {
+        this.brojStanovika = brojStanovika;
+    }
+
+    public DrzavaDTO getDrzava() {
+        return drzava;
+    }
+
+    public void setDrzava(DrzavaDTO drzava) {
+        this.drzava = drzava;
+    }
 }
