232ec45d40efdbb4c177462da2cf05afcdbbe0ea
amirnakic <anakic1@etf.unsa.ba>
2018-12-24 02:06:33 +0100

    Dodana je metoda nadjiDrzavu.

--- src/ba/unsa/etf/rpr/GeografijaDAO.java
+++ src/ba/unsa/etf/rpr/GeografijaDAO.java
@@ -1,8 +1,6 @@
 package ba.unsa.etf.rpr;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class GeografijaDAO {
     private static GeografijaDAO instance = null;
@@ -37,5 +35,27 @@ public class GeografijaDAO {
         }
         conn = null;
         instance = null;
+    }
+
+    public Drzava nadjiDrzavu(String drzava) {
+        try {
+            PreparedStatement stmt = conn.prepareStatement("SELECT drzava.naziv, grad.naziv, grad.broj_stanovnika FROM drzava, grad WHERE drzava.naziv = ? AND grad.id = drzava.id");
+            stmt.setString(1, drzava);
+            ResultSet rs = stmt.executeQuery();
+            if (rs.isClosed())
+                return null;
+            Drzava d = new Drzava();
+            d.setNaziv(rs.getString(1));
+            Grad g = new Grad();
+            g.setNaziv(rs.getString(2));
+            g.setBrojStanovika(rs.getInt(3));
+            d.setGlavniGrad(g);
+            g.setDrzava(d);
+            return d;
+        }
+        catch(SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
