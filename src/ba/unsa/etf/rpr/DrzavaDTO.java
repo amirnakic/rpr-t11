12f4e48a51f45e4478786f537bf2d2eefd6296bf
amirnakic <anakic1@etf.unsa.ba>
2018-12-24 01:15:55 +0100

    Implementirana je klasa DrzavaDTO tako da slijedi JavaBeans specifikacije.

--- src/ba/unsa/etf/rpr/DrzavaDTO.java
+++ src/ba/unsa/etf/rpr/DrzavaDTO.java
@@ -1,4 +1,30 @@
 package ba.unsa.etf.rpr;
 
 public class DrzavaDTO {
+    private String naziv;
+    private GradDTO glavniGrad;
+
+    public DrzavaDTO() {
+    }
+
+    public DrzavaDTO(String naziv, GradDTO glavniGrad) {
+        this.naziv = naziv;
+        this.glavniGrad = glavniGrad;
+    }
+
+    public String getNaziv() {
+        return naziv;
+    }
+
+    public void setNaziv(String naziv) {
+        this.naziv = naziv;
+    }
+
+    public GradDTO getGlavniGrad() {
+        return glavniGrad;
+    }
+
+    public void setGlavniGrad(GradDTO glavniGrad) {
+        this.glavniGrad = glavniGrad;
+    }
 }
