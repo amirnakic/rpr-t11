0fdbd35a0913f67e127f64180a295b8b72419de5
amirnakic <anakic1@etf.unsa.ba>
2018-12-27 23:42:24 +0100

    Dodan je osnovni izgled GUIja.

--- src/ba/unsa/etf/rpr/Controller.java
+++ src/ba/unsa/etf/rpr/Controller.java
@@ -1,4 +1,15 @@
 package ba.unsa.etf.rpr;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class Controller {
+    public TextField textField;
+    public Label glavniGradLbl;
+    public Button button;
+
+    public void clickOnButton(ActionEvent actionEvent) {
+    }
 }
