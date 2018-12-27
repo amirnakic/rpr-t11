40b644dcfc10e96c43562b13df9a5f47a3deea21
amirnakic <anakic1@etf.unsa.ba>
2018-12-27 23:49:39 +0100

    Implementiran je prosti GUI kojim tražimo glavni grad unesene države. U slučaju da unesena država nije prisutna u bazi podataka, ispisujemo "ERROR!".

--- src/ba/unsa/etf/rpr/Controller.java
+++ src/ba/unsa/etf/rpr/Controller.java
@@ -1,15 +1,28 @@
 package ba.unsa.etf.rpr;
 
 import javafx.event.ActionEvent;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import java.io.File;
 
 public class Controller {
     public TextField textField;
     public Label glavniGradLbl;
-    public Button button;
+    public GeografijaDAO dao;
+
+    public Controller() {
+        GeografijaDAO.removeInstance();
+        File dbfile = new File("baza.db");
+        dbfile.delete();
+        dao = GeografijaDAO.getInstance();
+    }
 
     public void clickOnButton(ActionEvent actionEvent) {
+        Grad g = dao.glavniGrad(textField.getText());
+        if (g == null) {
+            glavniGradLbl.setText("ERROR!");
+            return;
+        }
+        glavniGradLbl.setText(g.getNaziv().toUpperCase());
     }
 }
